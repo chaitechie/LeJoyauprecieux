29decbd7970be75f64bff1e33f9a224ad31cb21a
चैतन्य <Chaitanyavankar0@gmail.com>
2025-02-15 17:46:09 +0530

    Update firebase.config.ts

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -7,13 +7,13 @@ import {getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain:  process.env.AUTH_DOMAIN,
-  projectId:  process.env.PROJECT_ID,
-  storageBucket:  process.env.STORAGE_BUCKET,
-  messagingSenderId:  process.env.MESSAGE_SENDER_ID,
-  appId:  process.env.APP_ID,
-  measurementId:  process.env.MEASURED_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain:  import.meta.env.VITE_AUTH_DOMAIN,
+  projectId:  import.meta.env.VITE_PROJECT_ID,
+  storageBucket:  import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId:  import.meta.env.VITE_APP_ID,
+  measurementId:  import.meta.env.VITE_MEASURED_ID
 };
 
 // Initialize Firebase
